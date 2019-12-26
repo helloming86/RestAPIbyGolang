3147c359a161c87aec0a4c8b3db93aab0efd5ae1
Nicky <helloming86@gmail.com>
2019-12-26 23:08:54 +0800

    用HTTPS加密API请求

--- main.go
+++ main.go
@@ -61,22 +61,27 @@ func main() {
 		)
 
 	// Ping the server to make sure the router is working.
-	// 注意：go func() 声明在log.Printf()之前 标注1 声明在标注2、3前 为什么？
 	go func() {
 		if err := pingServer(); err != nil {
 			log.Fatal("The router has no response, or it might took too long to start up.", err)
 		}
 		log.Print("The router has been deployed successfully.")
-	}() // 标注1
-	// 让main goroutine 暂停运行500ms
-	// 这时，标注2和3不会执行，但是 go gunc 会并发继续执行，因为WEB服务还未启动，提示 Waiting for the router, retry in 1 second.
-	// main goroutine 暂停运行时间一过，标注2、3继续执行
-	// 标注2 执行后，提示：Start to listening the incoming requests on http address: :8080
-	// 标注3，成功启动WEB服务
-	// 并发 go gunc 在WEB服务启动并没有Error的情况下，提示： The router has been deployed successfully.
-	time.Sleep(time.Millisecond * 500)
-	log.Printf("Start to listening the incoming requests on http address: %s", viper.GetString("addr")) // 标注2
-	log.Printf(http.ListenAndServe(viper.GetString("addr"), g).Error())  // 标注3
+	}()
+
+	// 如果提供了 TLS 证书和私钥则启动 HTTPS 端口。
+	cert := viper.GetString("tls.cert")
+	key := viper.GetString("tls.key")
+
+	if cert != "" && key != "" {
+		go func() {
+			log.Printf("Start to listening the incoming requests on http address: %s", viper.GetString("tls.addr"))
+			log.Printf(http.ListenAndServeTLS(viper.GetString("tls.addr"), cert, key, g).Error())
+		}()
+	}
+
+	// 启动HTTP端口
+	log.Printf("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
+	log.Printf(http.ListenAndServe(viper.GetString("addr"), g).Error())
 }
 
 // pingServer pings the http server to make sure the router is working.
@@ -93,3 +98,4 @@ func pingServer() error {
 	}
 	return errors.New("Cannot connect to the router.")
 }
+
